74b942b4f6a6c132dc4f7f01a9a121f6b33cf0f7
Patrick Hanson <patrickhanson08@gmail.com>
2018-12-03 13:23:17 -0500

    fixed pep8 errors

--- xword.py
+++ xword.py
@@ -6,22 +6,24 @@ import re
 
 __author__ = "patrickhanson"
 
+
 # YOUR HELPER FUNCTION GOES HERE
 def find_words(user_input, dictionary):
-    input_word = user_input.replace(' ', '\w')
+    input_word = user_input.replace(' ', '.')
     regex_word = re.compile(input_word)
     for word in dictionary:
         if len(user_input) == len(word):
             if re.match(regex_word, word):
                 print word
-                
+
 
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
 
-    test_word = raw_input(
-        'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
+    test_word = raw_input("""
+        'Please enter a word to solve.
+        Use spaces to signify unknown letters: """).lower()
 
     find_words(test_word, words)
     # YOUR ADDITIONAL CODE HERE
